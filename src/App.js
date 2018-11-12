b2dcbda52c47aa636b04ca8aaa391677f3d7bf53
berwin han <berwin0415@gmail.com>
2018-11-12 22:48:54 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>韩博文的react-demo</div>
+        <ul>
+          <li>哈哈哈哈哈哈哈哈</li>
+          <li>哈哈哈哈哈哈哈哈</li>
+          <li>哈哈哈哈哈哈哈哈</li>
+          <li>哈哈哈哈哈哈哈哈</li>
+          <li>哈哈哈哈哈哈哈哈</li>
+          <li>哈哈哈哈哈哈哈哈</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
