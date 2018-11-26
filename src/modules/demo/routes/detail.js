a775868746aef75a7e9bfdda5cb9f2a0bc2dfdf5
韩博文 <hanbowen@hanbowendeMacBook-Pro.local>
2018-11-26 09:49:06 +0800

    init bundle

--- src/modules/demo/routes/detail.js
+++ src/modules/demo/routes/detail.js
@@ -1,9 +1,17 @@
-import Detail from '../pages/detail'
+import React from "react";
+
+import Bundle from "Client/bundle";
+
+const Detail = props => (
+  <Bundle load={() => import("../pages/detail")}>
+    {Detail => <Detail {...props} />}
+  </Bundle>
+);
 
 const route = {
-    title: '详情页',
-    path: '/detail',
-    exact: true,
-    component: Detail
-}
+  title: "详情页",
+  path: "/detail",
+  exact: true,
+  component: Detail
+};
 export default route;
